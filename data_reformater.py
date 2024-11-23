85cb887eb6537aae29d10ea4b40110235cabaae7
Kevin-Chen-NYU <124224643+Kevin-Chen-NYU@users.noreply.github.com>
2024-11-22 23:29:09 -0500

    Update data_reformater.py

--- data_reformater.py
+++ data_reformater.py
@@ -1,4 +1,8 @@
 import sys
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.tag import pos_tag
+
 
 if (len(sys.argv) != 4):
     print("Not enough arguments")
@@ -15,21 +19,27 @@ else:
                 print(char , end = "", file=temp_output)
                 print(" ", end = "", file=temp_output)
             else:
-                print(char , end = "", file=temp_output)
+                print(char , end = "", file=temp_output)      
     temp_output.close()
     file = open("temp.txt", "r")
+
     if (sys.argv[2] == "training"):
-        
         t_output = open("training.POS-chunk", "w")
-        
+        count = 0
         for line in file:
+            count = 0
             if (not line.isspace()):
                 sentence = line.split()
-                print(sentence , file=t_output)
                 inside_label_type = "false"
                 current_type = ""
                 inside_label = "false"
                 begin = "false"
+                no_label_sentence = ""
+                for index in range(1, len(sentence)):
+                    if (("<" not in sentence[index]) and (">" not in sentence[index])):
+                        no_label_sentence = no_label_sentence + " " + sentence[index]
+                tokenized = word_tokenize(no_label_sentence)
+                tagged_sentence = nltk.pos_tag(tokenized)        
                 for word in sentence:
                     if (word != sentence[0]):
                         if ((word[0] == "<") and (word[1] != "/")):
@@ -39,27 +49,89 @@ else:
                             inside_label_type = "false"
                             inside_label = "true"
                             begin = "true"
-                        elif ((word[0] != "<") and (inside_label == "true")):
+                        elif (inside_label == "true"):
                             if (begin == "true"):
-                                print(word + "\t" + "B-" + current_type, file=t_output)
+                                if (word_group in current_type):
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "B-" + word_group, file=t_output)
+                                    count = count + 1
+                                else:
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "O", file=t_output)
+                                    count = count + 1
                                 begin = "false"
                             elif (word[0:2]!= "</"):
-                                print(word + "\t" + "I-" + current_type, file=t_output)
+                                if (word_group in current_type):
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "I-" + word_group, file=t_output)
+                                    count = count + 1
+                                else:
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "O", file=t_output)
+                                    count = count + 1
                             elif (word[0:2]== "</"):
                                 inside_label = "false"
                                 current_type = ""
-                                
-                                
-                                                
-                        
-                
+                        else:
+                            print(word + "\t" + tagged_sentence[count][1] + "\t" + "O", file=t_output)
+                            count = count + 1
+                print("", file=t_output) 
         t_output.close()
+
         
-    elif (sys.argv[2] == others):
-        print("Not yet implemented")
+    elif (sys.argv[2] == "other"):
+        answer_key = open("dev_or_test.POS-chunk", "w")
+        o_output = open("dev_or_test.POS", "w")
+        count = 0
+        for line in file:
+            count = 0
+            if (not line.isspace()):
+                sentence = line.split()
+                inside_label_type = "false"
+                current_type = ""
+                inside_label = "false"
+                begin = "false"
+                no_label_sentence = ""
+                for index in range(1, len(sentence)):
+                    if (("<" not in sentence[index]) and (">" not in sentence[index])):
+                        no_label_sentence = no_label_sentence + " " + sentence[index]
+                tokenized = word_tokenize(no_label_sentence)
+                tagged_sentence = nltk.pos_tag(tokenized)        
+                for word in sentence:
+                    if (word != sentence[0]):
+                        if ((word[0] == "<") and (word[1] != "/")):
+                            inside_label_type = "true"
+                        elif (inside_label_type == "true"):
+                            current_type = word[0:-1]
+                            inside_label_type = "false"
+                            inside_label = "true"
+                            begin = "true"
+                        elif (inside_label == "true"):
+                            if (begin == "true"):
+                                if (word_group in current_type):
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "B-" + word_group, file=answer_key)
+                                    print(word + "\t" + tagged_sentence[count][1], file=o_output)
+                                    count = count + 1
+                                else:
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "O", file=answer_key)
+                                    print(word + "\t" + tagged_sentence[count][1], file=o_output)
+                                    count = count + 1
+                                begin = "false"
+                            elif (word[0:2]!= "</"):
+                                if (word_group in current_type):
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "I-" + word_group, file=answer_key)
+                                    print(word + "\t" + tagged_sentence[count][1], file=o_output)
+                                    count = count + 1
+                                else:
+                                    print(word + "\t" + tagged_sentence[count][1] + "\t" + "O", file=answer_key)
+                                    print(word + "\t" + tagged_sentence[count][1], file=o_output)
+                                    count = count + 1
+                            elif (word[0:2]== "</"):
+                                inside_label = "false"
+                                current_type = ""
+                        else:
+                            print(word + "\t" + tagged_sentence[count][1] + "\t" + "O", file=answer_key)
+                            print(word + "\t" + tagged_sentence[count][1], file=o_output)
+                            count = count + 1
+                print("", file=answer_key)
+                print("", file=t_output)
+        answer_key.close()
+        o_output.close()
 
     file.close()
-        
-
-        
-#python3 data_reformater.py training_data_combined.txt training date
